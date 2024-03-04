f011c9d625b3cd40f08ca6c1821ae70fbfa5a29a
Matt <cavallo5689@gmail.com>
2024-03-04 11:20:33 -0600

    first

--- Program.cs
+++ Program.cs
@@ -18,3 +18,39 @@ internal class Program
         host.Run();
     }
 }
+
+public interface IMyService
+{
+    public void MyServiceMethod();
+}
+
+public class MyService : IMyService
+{
+    public IServiceCollection? _serviceCollection;
+    public IServiceProvider? _serviceProvider;
+
+    public MyService()
+    {
+        this.CreateServiceCollection();
+        // Use AddHttpClient here or in the MyService class
+    }
+
+    public void CreateServiceCollection()
+    {
+        _serviceCollection = new ServiceCollection();
+
+        _serviceCollection.AddHttpClient();
+        _serviceCollection.AddAuthentication();
+
+        _serviceProvider = _serviceCollection.BuildServiceProvider();
+    }
+
+    public void MyServiceMethod()
+    {
+        var myService = _serviceProvider?.GetRequiredService<IHttpClientFactory>();
+
+        var myClient = myService.CreateClient();
+
+        System.Console.WriteLine("Hello from MyServiceMethod()!");
+    }
+}
